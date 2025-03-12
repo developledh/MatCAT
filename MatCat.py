114ec9b7bb67d3d05ea83ba6190b921e56d206eb
Lee Dong Ho <105855716+developledh@users.noreply.github.com>
2025-03-12 23:48:25 +0900

    Update MatCat.py

--- MatCat.py
+++ MatCat.py
@@ -1,5 +1,7 @@
 import os
-
+import glob
+import shutil
+import ase.io as io
 class MatCat:
     @staticmethod
     def write_pbs_script(modelname):
@@ -30,8 +32,54 @@ module add fftw_mpi/3.3.7
 
 cd $PBS_O_WORKDIR
 
+# Define the path to the POTCAR directory
+# Recommended potentials dictionary in a more compact format
+declare -A recommended_potentials=(
+    ["H"]="H" ["He"]="He" ["Li"]="Li_sv" ["Be"]="Be_sv" ["B"]="B" ["C"]="C" ["N"]="N"
+    ["O"]="O" ["F"]="F" ["Ne"]="Ne" ["Na"]="Na_pv" ["Mg"]="Mg" ["Al"]="Al" ["Si"]="Si"
+    ["P"]="P" ["S"]="S" ["Cl"]="Cl" ["Ar"]="Ar" ["K"]="K_sv" ["Ca"]="Ca_sv" ["Sc"]="Sc_sv"
+    ["Ti"]="Ti_sv" ["V"]="V_sv" ["Cr"]="Cr_pv" ["Mn"]="Mn_pv" ["Fe"]="Fe" ["Co"]="Co"
+    ["Ni"]="Ni" ["Cu"]="Cu" ["Zn"]="Zn" ["Ga"]="Ga_d" ["Ge"]="Ge_d" ["As"]="As"
+    ["Se"]="Se" ["Br"]="Br" ["Kr"]="Kr" ["Rb"]="Rb_sv" ["Sr"]="Sr_sv" ["Y"]="Y_sv"
+    ["Zr"]="Zr_sv" ["Nb"]="Nb_sv" ["Mo"]="Mo_sv" ["Tc"]="Tc_pv" ["Ru"]="Ru_pv" ["Rh"]="Rh_pv"
+    ["Pd"]="Pd" ["Ag"]="Ag" ["Cd"]="Cd" ["In"]="In_d" ["Sn"]="Sn_d" ["Sb"]="Sb"
+    ["Te"]="Te" ["I"]="I" ["Xe"]="Xe" ["Cs"]="Cs_sv" ["Ba"]="Ba_sv" ["La"]="La"
+    ["Ce"]="Ce" ["Pr_3"]="Pr_3"
+    ["Nd"]="Nd_3" ["Pm"]="Pm_3" ["Sm"]="Sm_3" ["Eu"]="Eu_2"
+    ["Gd"]="Gd_3" ["Tb"]="Tb_3" ["Dy"]="Dy_3" ["Ho"]="Ho_3" ["Er"]="Er_3"
+    ["Tm"]="Tm_3" ["Yb"]="Yb_2" ["Lu"]="Lu_3" ["Hf"]="Hf_pv"
+    ["Ta"]="Ta_pv" ["W"]="W_sv" ["Re"]="Re" ["Os"]="Os" ["Ir"]="Ir" ["Pt"]="Pt"
+    ["Au"]="Au" ["Hg"]="Hg" ["Tl"]="Tl_d" ["Pb"]="Pb_d"
+    ["Bi"]="Bi_d" ["Po"]="Po_d" ["At"]="At" ["Rn"]="Rn"
+    ["Fr"]="Fr_sv" ["Ra"]="Ra_sv" ["Ac"]="Ac" ["Th"]="Th" ["Pa"]="Pa"
+    ["U"]="U" ["Np"]="Np" ["Pu"]="Pu"
+    ["Am"]="Am" ["Cm"]="Cm" ["Cf"]="Cf"
+)
+
+# Function to create the POTCAR file based on the elements in the POSCAR
+function create_potcar {{
+    elements=$(awk 'NR==6 {{print $0}}' POSCAR)
+    rm -f POTCAR
+    for element in $elements; do
+        potcar_file=${{recommended_potentials[$element]}}
+        if [ -z "$potcar_file" ]; then
+            echo "No recommended POTCAR for element $element"
+            exit 1
+        fi
+        if [ -f $POTCAR_DIR/$potcar_file/POTCAR ]; then
+            cat $POTCAR_DIR/$potcar_file/POTCAR >> POTCAR
+        else
+            echo "POTCAR for element $element ($potcar_file) not found in $POTCAR_DIR"
+            exit 1
+        fi
+    done
+}}
+
+# Create the POTCAR file
+create_potcar
+
 # VASP PART 
-time mpirun /home01/x3083a05/bin/vasp_std > vasp-.out
+time mpirun /home01/x3083a0/bin/vasp_std > vasp-.out
 
 echo "Closing Time is $(date)"
 
@@ -47,7 +95,7 @@ time python optimization_to_band.py
 ''')
 
     @staticmethod
-    def write_incar(directory):
+    def write_incar(directory):           ## You write your INCAR option depending on your target material
         incar_content = """
 ALGO = Normal
 EDIFF = 0.002
@@ -89,57 +137,16 @@ LCHARG = .False.
         print(f"Modified {incar_file} with MAGMOM = {total_atoms}*0.6")
 
     @staticmethod
-    def write_kpoints(directory):
+    def write_kpoints(directory):     ## You write your KPOINTS depending on your target material 
         kpoints_content = """
 kpoints
 0
 gamma
-8 8 8
+4 4 4
 0 0 0"""
         kpoints_path = os.path.join(directory, "KPOINTS")
         with open(kpoints_path, "w") as f:
             f.write(kpoints_content)
-
-    @staticmethod
-    def create_potcar():
-        poscar_file = "POSCAR"
-        potcar_file = "POTCAR"
-        potcar_dir = os.getenv("POTCAR_DIR")
-        
-        if not potcar_dir:
-            print("Environment variable POTCAR_DIR is not set.")
-            return
-
-        recommended_potentials = {
-            "H": "H", "He": "He", "Li": "Li_sv", "Be": "Be_sv", "B": "B", "C": "C", "N": "N",
-            "O": "O", "F": "F", "Ne": "Ne", "Na": "Na_pv", "Mg": "Mg", "Al": "Al", "Si": "Si",
-            "P": "P", "S": "S", "Cl": "Cl", "Ar": "Ar", "K": "K_sv", "Ca": "Ca_sv", "Sc": "Sc_sv",
-            "Ti": "Ti_sv", "V": "V_sv", "Cr": "Cr_pv", "Mn": "Mn_pv", "Fe": "Fe", "Co": "Co",
-            "Ni": "Ni", "Cu": "Cu", "Zn": "Zn", "Ga": "Ga_d", "Ge": "Ge_d", "As": "As",
-            "Se": "Se", "Br": "Br", "Kr": "Kr", "Rb": "Rb_sv", "Sr": "Sr_sv", "Y": "Y_sv",
-            "Zr": "Zr_sv", "Nb": "Nb_sv", "Mo": "Mo_sv", "Tc": "Tc_pv", "Ru": "Ru_pv", "Rh": "Rh_pv",
-            "Pd": "Pd", "Ag": "Ag", "Cd": "Cd", "In": "In_d", "Sn": "Sn_d", "Sb": "Sb",
-            "Te": "Te", "I": "I", "Xe": "Xe", "Cs": "Cs_sv", "Ba": "Ba_sv", "La": "La"
-        }
-        
-        with open(poscar_file, 'r') as f:
-            lines = f.readlines()
-            elements = lines[5].split()
-        
-        with open(potcar_file, 'w') as potcar:
-            for element in elements:
-                if element in recommended_potentials:
-                    pot_path = os.path.join(potcar_dir, recommended_potentials[element], "POTCAR")
-                    if os.path.exists(pot_path):
-                        with open(pot_path, 'r') as p:
-                            potcar.write(p.read())
-                    else:
-                        print(f"POTCAR for element {element} not found in {pot_path}")
-                        return
-                else:
-                    print(f"No recommended POTCAR for element {element}")
-                    return
-        print("POTCAR file successfully created.")
 
     @staticmethod
     def convert_cif_to_poscar(cif_folder="./cifs", output_folder="."):
@@ -164,14 +171,29 @@ gamma
             os.makedirs(optimization_path, exist_ok=True)
             os.rename(poscar_path, os.path.join(optimization_path, "POSCAR"))
 
-
     @staticmethod
     def copy_bin(destination_folder):
+        """ Copies the 'bin' directory from MatCat into the specified folder, avoiding duplicate files. """
         source_bin = os.path.join(os.path.dirname(__file__), "bin")
         target_bin = os.path.join(destination_folder, "bin")
-        
-        if os.path.exists(source_bin):
-            shutil.copytree(source_bin, target_bin, dirs_exist_ok=True)
-            print(f"Copied bin directory to: {target_bin}")
+
+        if not os.path.exists(source_bin):
+            print(f"Source bin directory does not exist: {source_bin}")
+            return
+
+        if not os.path.exists(target_bin):
+            shutil.copytree(source_bin, target_bin)  # If folder doesn't exist, make the folder. 
         else:
-            print("bin directory not found in MatCat package.")
+            print(f"Bin folder already exists in {destination_folder}. Updating files...")
+
+            for item in os.listdir(source_bin):
+                src_item = os.path.join(source_bin, item)
+                dest_item = os.path.join(target_bin, item)
+
+                if os.path.isdir(src_item):
+                    shutil.copytree(src_item, dest_item, dirs_exist_ok=True)
+                
+                elif not os.path.exists(dest_item):
+                    shutil.copy2(src_item, dest_item)
+
+        print(f"Copied bin directory to: {target_bin}")
